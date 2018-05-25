4a8ed3581f5470bfcbb451c3cf14763a838de78f
Gilbert Röhrbein <gilbert@ifsr.de>
2018-05-25 16:46:12 +0200

    oh

--- tests/sccache_msvc_ninja.rs
+++ tests/sccache_msvc_ninja.rs
@@ -7,7 +7,6 @@ use std::process::{Command};
 
 use testutils::{find_sccache_binary};
 
-// - cmd: cd tests\test-cmake && cmake . -G Ninja -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_C_COMPILER=cl.exe && cmake --build .
 #[test]
 fn test_cmake_configure() {
     let mut here = std::env::current_dir().unwrap();
@@ -48,6 +47,64 @@ fn test_cmake_configure() {
 
     Command::new("cmd")
         .args(&["/C", "test_cmake_configure.bat"])
+        .current_dir("tests")
+        .env("CL_EXE_DIR", &here)
+        .status()
+        .unwrap();
+
+    Command::new(sccache)
+        .arg("--show-stats")
+        .status()
+        .unwrap();
+}
+
+#[test]
+fn test_cmake_build() {
+    let mut here = std::env::current_dir().unwrap();
+    let here_rel = Path::new(file!()).parent().unwrap();
+    here.push(here_rel);
+    
+    let cl_exe = here.join("cl.exe");
+    let sccache_path = find_sccache_binary();
+    let sccache = sccache_path.to_str().unwrap();
+    
+    Command::new(sccache)
+        .arg("--stop-server")
+        .output()
+        .unwrap();
+    Command::new(sccache)
+        .arg("--start-server")
+        .status()
+        .unwrap();
+    Command::new(sccache)
+        .arg("--show-stats")
+        .status()
+        .unwrap();
+
+    fs::hard_link(sccache, cl_exe)
+        .unwrap_or_default();
+
+    Command::new("cmd")
+        .args(&["/C", "test_cmake_configure.bat"])
+        .current_dir("tests")
+        .env("CL_EXE_DIR", &here)
+        .status()
+        .unwrap();
+
+    Command::new("cmd")
+        .args(&["/C", "test_cmake_build.bat"])
+        .current_dir("tests")
+        .env("CL_EXE_DIR", &here)
+        .status()
+        .unwrap();
+
+    Command::new(sccache)
+        .arg("--show-stats")
+        .status()
+        .unwrap();
+
+    Command::new("cmd")
+        .args(&["/C", "test_cmake_build.bat"])
         .current_dir("tests")
         .env("CL_EXE_DIR", &here)
         .status()
--- tests/test_cmake_configure.bat
+++ tests/test_cmake_configure.bat
@@ -5,15 +5,15 @@ set vcvarsall_15C="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
 set vcvarsall_15P="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat"
 set vcvarsall_15E="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 
-cd test-cmake
-rmdir /s/q build
-mkdir build
-cd build
+echo recreate test-cmake\build
+rmdir /s/q test-cmake\build
+mkdir test-cmake\build
+cd test-cmake\build
 
 if        exist %vcvarsall_15E% ( call %vcvarsall_15E% x64
-) else if exist %vcvarsall_15P% ( %vcvarsall_15P% x64
-) else if exist %vcvarsall_15C% ( %vcvarsall_15C% x64
-) else if exist %vcvarsall_14% ( %vcvarsall_14% x64
+) else if exist %vcvarsall_15P% ( call %vcvarsall_15P% x64
+) else if exist %vcvarsall_15C% ( call %vcvarsall_15C% x64
+) else if exist %vcvarsall_14% ( call %vcvarsall_14% x64
 ) else (
     echo Could not find a Visual Studio vcvarsall.
     echo Please edit this script or run it after running vcvarsall manually.
@@ -22,10 +22,6 @@ if        exist %vcvarsall_15E% ( call %vcvarsall_15E% x64
 
 set PATH=%CL_EXE_DIR%;%PATH%
 
-where cl
-echo %PATH%
-
-rmdir *
 cmake .. -G Ninja -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_C_COMPILER=cl.exe
 
 cd ..
